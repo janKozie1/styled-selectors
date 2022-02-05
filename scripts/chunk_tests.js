ed85f97a59a8f4260713d6c8e5b4c237598967a9
janKozie1 <jankkoziel@gmail.com>
2022-02-05 22:40:15 +0100

    test

--- scripts/chunk_tests.js
+++ scripts/chunk_tests.js
@@ -1,9 +1,31 @@
 const { exec } = require('child_process');
 
-function init() {
+const promisify = (fn) => {
+  return (...args) => new Promise((resolve, reject) => {
+    try {
+      fn(...args, (error, result) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(result)
+        }
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+async function init() {
   console.log(process.argv);
 
-  console.log(exec('ls'))
+  const asyncExec = promisify(exec);
+
+  asyncExec('pwd').then((result) => console.log(result))
+
+  asyncExec('find ./src -name "*.spec.ts"')
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err))
 }
 
 init();
