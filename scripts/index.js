928b1fa71c3d01615fe0f2b7e5d6c86b30410930
janKozie1 <jankkoziel@gmail.com>
2022-02-06 18:22:10 +0100

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,19 @@
 const { exec } = require('child_process');
-const { relative } = require('path')
+const { relative } = require('path');
 
-const promisify = (fn) => {
-  return (...args) => new Promise((resolve, reject) => {
-    try {
-      fn(...args, (error, result) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(result)
-        }
-      })
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
+const promisify = (fn) => (...args) => new Promise((resolve, reject) => {
+  try {
+    fn(...args, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  } catch (err) {
+    reject(err);
+  }
+});
 
 async function init() {
   const asyncExec = promisify(exec);
@@ -23,15 +21,9 @@ async function init() {
   const token = process.argv[2];
   await asyncExec(`echo ${token} | gh auth login --with-token`);
 
-  const info = await asyncExec(`gh pr view`);
-
-  const response = info.toString();
-
-  console.log({response})
-  const prDetails = response.match(/into (\S+) from (\S+)/i);
-  const [, baseBranch, targetBranch] = prDetails;
-  console.log("Base branch: ", baseBranch);
-  console.log("Target branch: ", targetBranch);
+  const {baseRefName, headRefName} = JSON.parse(await asyncExec(`gh pr view  --json baseRefName,headRefName`));
+  console.log("Base branch: ", baseRefName);
+  console.log("Target branch: ", headRefName);
 }
 
 init()
