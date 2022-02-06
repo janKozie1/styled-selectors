f3ca2844a5849130cfea6e04dd867cd6d1960f13
janKozie1 <jankkoziel@gmail.com>
2022-02-06 18:35:07 +0100

    test

--- scripts/index.js
+++ scripts/index.js
@@ -34,8 +34,8 @@ const main = async() => {
   const token = process.argv[2];
   await exec(`echo ${token} | gh auth login --with-token`);
 
-  const { baseBranch } = await getPRBranches();
-  await fetchBaseBranch(baseBranch)
+  const branches = await getPRBranches();
+  await fetchBaseBranch(branches.to)
 }
 
 main()
