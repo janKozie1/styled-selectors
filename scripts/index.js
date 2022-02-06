f1f20306722b9acb08a24554cb539b6d4e6e9440
janKozie1 <jankkoziel@gmail.com>
2022-02-06 18:31:33 +0100

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-const { exec } = require('child_process');
-const { relative } = require('path');
+const defaultExec = require('child_process').exec;
 
 const promisify = (fn) => (...args) => new Promise((resolve, reject) => {
   try {
@@ -15,18 +14,31 @@ const promisify = (fn) => (...args) => new Promise((resolve, reject) => {
   }
 });
 
-async function init() {
-  const asyncExec = promisify(exec);
+const exec = promisify(defaultExec);
 
+const getPRBranches = async () => {
+  const { prBranch, baseBranch } = JSON.parse(await exec(`gh pr view  --json baseRefName,headRefName`));
+  return { to: baseBranch, from: prBranch }
+}
+
+const fetchBaseBranch = async (baseBranch) => {
+  const addBaseRefs = `git config --add remote.origin.fetch +refs/heads/${baseBranch}:refs/remotes/origin/${baseBranch}`;
+  const fetchBase = `git fetch --no-tags origin +refs/heads/${baseBranch}:refs/remotes/origin/${baseBranch}`;
+  const command = `${addBaseRefs} && ${fetchBase}`;
+
+  await exec(command);
+}
+
+
+const main = async() => {
   const token = process.argv[2];
   await asyncExec(`echo ${token} | gh auth login --with-token`);
 
-  const {baseRefName, headRefName} = JSON.parse(await asyncExec(`gh pr view  --json baseRefName,headRefName`));
-  console.log("Base branch: ", baseRefName);
-  console.log("Target branch: ", headRefName);
+  const { baseBranch } = await getPRBranches();
+  await fetchBaseBranch(baseBranch)
 }
 
-init()
+main()
   .then((chunks) => console.log(chunks))
   .catch((err) => {
     console.log(err, "??")
